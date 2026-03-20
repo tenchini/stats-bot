48a85eccce7216e4f5b2a0e9b8d11c5941b9e23b
tenchini <viniciustenchi@gmail.com>
2026-03-20 14:44:12 -0300

    feat: estrutura inicial do projeto com Spring Boot e JDA

---
+++ src/main/java/com/cs2bot/stats_bot/StatsBotApplication.java
@@ -0,0 +1,13 @@
+package com.cs2bot.stats_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs2bot/stats_bot/StatsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs2bot.stats_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
